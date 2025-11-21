e06e240628ed13fbadb8ed3ecd047b7bc7ea32d2
Bruno Felix <brunofelixpinto@gmail.com>
2025-11-21 17:21:59 -0300

    feat: startando frontend

---
+++ ProjetoFinal/Frontend/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo ao React</h1>
+        <p>
+          Edite <code>src/App.js</code> e salve para recarregar.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Aprenda React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
